c92d0ef1e3bb9568c8ef9790a99bdb97eba7cfab
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-02-10 14:11:53 -0800

    Temporary for mobile->xulrunner switch regression, need to figure out why SessionStore helping to avoid mispositioned rendering on startup

--- link_to_system.sh
+++ link_to_system.sh
@@ -21,6 +21,7 @@ EmbedLiteJSComponents.manifest
 jscomps/AboutRedirector.js
 jscomps/AlertsService.js
 jscomps/LoginManagerPrompter.js
+jscomps/SessionStore.js
 "
 for str in $FILES_LIST; do
     fname="${str##*/}"
