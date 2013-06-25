69ad15c88a25b38a815967eaf363a7e0611a0922
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-06-24 23:23:40 -0700

    Added stubs for fetch all possible firefox store data

--- jscomps/EmbedLiteSyncService.js
+++ jscomps/EmbedLiteSyncService.js
@@ -22,6 +22,11 @@ function EmbedLiteSyncServiceImpotUtils()
     Cu.import("resource://services-sync/engines.js");
     Cu.import("resource://services-sync/record.js");
     Cu.import("resource://services-sync/engines/history.js");
+    Cu.import("resource://services-sync/engines/apps.js");
+    Cu.import("resource://services-sync/engines/forms.js");
+    Cu.import("resource://services-sync/engines/passwords.js");
+    Cu.import("resource://services-sync/engines/prefs.js");
+    Cu.import("resource://services-sync/engines/tabs.js");
     Cu.import("chrome://embedlite/content/sync/bookmarks.js");
 }
 
@@ -40,7 +45,7 @@ EmbedLiteSyncService.prototype = {
       // Engine DownloadManager notifications
       case "app-startup": {
         dump("EmbedLiteSyncService app-startup\n");
-        Services.prefs.setCharPref("services.sync.registerEngines", "Bookmarks,History");
+        Services.prefs.setCharPref("services.sync.registerEngines", "Tab,Bookmarks,Form,History,Password,Prefs");
         Services.obs.addObserver(this, "embedui:initsync", true);
         break;
       }
@@ -51,39 +56,60 @@ EmbedLiteSyncService.prototype = {
         Service.login(data.username, data.password, data.key);
         //this.embedLiteSyncServiceFetchBookmarks();
         //this.embedLiteSyncServiceFetchHistory();
-
+        //this.embedLiteSyncServiceFetchTabs();
+        //this.embedLiteSyncServiceFetchForms();
+        //this.embedLiteSyncServiceFetchPassword();
+        //this.embedLiteSyncServiceFetchPrefs();
         break;
       }
     }
   },
 
-  embedLiteSyncServiceFetchBookmarks: function () {
-    let collection = "bookmarks";
-    let key = Service.collectionKeys.keyForCollection(this.name);
-    let coll = new Collection(Service.storageURL + collection, PlacesItem, Service);
+  _embedLiteSyncServiceFetch: function (collection, Type, callback) {
+    let key = Service.collectionKeys.keyForCollection(collection);
+    let coll = new Collection(Service.storageURL + collection, Type, Service);
     coll.full = true;
     coll.recordHandler = function(item) {
       item.collection = collection;
       item.decrypt(key);
-      if (item.cleartext.type == "bookmark") {
-        let decobj = item.cleartext;
-        dump("Title: " + decobj.title + ", Uri: " + decobj.bmkUri + "\n");
-      }
+      callback(item.cleartext);
     };
     coll.get();
   },
 
+  embedLiteSyncServiceFetchBookmarks: function () {
+    this._embedLiteSyncServiceFetch("bookmarks", PlacesItem, function(item) {
+      if (item.type == "bookmark") {
+        dump("Title: " + item.title + ", Uri: " + item.bmkUri + "\n");
+      }
+    });
+  },
+
   embedLiteSyncServiceFetchHistory: function () {
-    let collection = "history";
-    let key = Service.collectionKeys.keyForCollection(this.name);
-    let coll = new Collection(Service.storageURL + collection, HistoryRec, Service);
-    coll.full = true;
-    coll.recordHandler = function(item) {
-      item.collection = collection;
-      item.decrypt(key);
-      dump("Title: " + item.cleartext.title + ", Uri:" + item.cleartext.histUri + "\n");
-    };
-    coll.get();
+    this._embedLiteSyncServiceFetch("history", HistoryRec, function(item) {
+      dump("Title: " +  item.title + ", Uri:" +  item.histUri + "\n");
+    });
+  },
+
+  embedLiteSyncServiceFetchTabs: function () {
+    this._embedLiteSyncServiceFetch("tabs", TabSetRecord, function(item) {
+      dump('Tab:' + JSON.stringify(item) + "\n");
+    });
+  },
+  embedLiteSyncServiceFetchForms: function () {
+    this._embedLiteSyncServiceFetch("forms", FormRec, function(item) {
+      dump('Forms:' + JSON.stringify(item) + "\n");
+    });
+  },
+  embedLiteSyncServiceFetchPassword: function () {
+    this._embedLiteSyncServiceFetch("passwords", LoginRec, function(item) {
+      dump('Login:' + JSON.stringify(item) + "\n");
+    });
+  },
+  embedLiteSyncServiceFetchPrefs: function () {
+    this._embedLiteSyncServiceFetch("prefs", PrefRec, function(item) {
+      dump('Pref:' + JSON.stringify(item) + "\n");
+    });
   },
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsISupportsWeakReference])
