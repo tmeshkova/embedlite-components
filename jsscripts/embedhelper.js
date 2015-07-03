90179d5709ebd338739938f44415cf62226ee9c0
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2015-07-03 16:17:31 +0300

    Drop unused subscription

--- jsscripts/embedhelper.js
+++ jsscripts/embedhelper.js
@@ -65,7 +65,6 @@ EmbedHelper.prototype = {
     addEventListener("DOMAutoComplete", this, true);
     addEventListener("blur", this, true);
     addEventListener("mozfullscreenchange", this, false);
-    addMessageListener("AZPC:ScrollDOMEvent", this);
     addMessageListener("Viewport:Change", this);
     addMessageListener("Gesture:DoubleTap", this);
     addMessageListener("Gesture:SingleTap", this);
